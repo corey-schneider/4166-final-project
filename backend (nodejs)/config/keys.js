8c92e570a6f4e6931fde930b5a12c7d7058e3d78
Corey Schneider <cschne11@uncc.edu>
2020-12-16 23:51:23 -0500

    testing gitignore

--- backend (nodejs)/config/keys.js
+++ backend (nodejs)/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://admin:372zzl7rmFSIUNVZ@cluster0.m5ybl.mongodb.net/database" 
+    mongoURI: "mongodb+srv://admin:372zzl7rmFSIUNVZ@cluster0.m5ybl.mongodb.net/database",
+    testingHi: "blah"
 };
