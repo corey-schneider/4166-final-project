e1107a57bf5b0b1c3bdab6f0c94b1772b671fe13
Corey <cschne11@uncc.edu>
2020-12-18 23:27:52 -0500

    testing gitignore

--- backend (nodejs)/config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://admin:372zzl7rmFSIUNVZ@cluster0.m5ybl.mongodb.net/database",
-    mongo_budgetURI: "mongodb+srv://admin:372zzl7rmFSIUNVZ@cluster0.m5ybl.mongodb.net/budget_db",
-    testingHi: "blah4"
-};
