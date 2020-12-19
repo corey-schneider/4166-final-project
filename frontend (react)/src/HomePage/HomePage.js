9e47a5613dac4af9f534d900ab75db33c83bc91e
Corey <cschne11@uncc.edu>
2020-12-18 23:21:01 -0500

    testing gitignore

--- frontend (react)/src/HomePage/HomePage.js
+++ frontend (react)/src/HomePage/HomePage.js
@@ -13,7 +13,7 @@ class HomePage extends Component {
 					<br/>
 				<h1>Please read before using this app!!!</h1>
 				<p>There seems to be an issue with login / registration after going to the dashboard.<br/>
-				<strong>Please DO NOT visit the dashboard UNTIL you have tested login and registration.</strong><br/>
+				<strong>Please DO NOT visit the dashboard UNTIL you have tested login and registration. Log in and Registration functions WILL NOT WORK if you visit the Dashboard BEFORE trying log in and register.</strong><br/>
 				I am still working on fixing this issue. Thank you!</p><br/><br/><br/>
 				<h4>This application was created using <a href="https://reactjs.org/" rel="nofollow">React</a>, <a href="https://nodejs.org/en/" rel="nofollow">Node.js</a>, and <a href="https://www.mongodb.com/" rel="nofollow">MongoDB</a></h4>
 			</>
